0769e32499c4bbec61ff8ea19921517e2cc077a5
Kuan Kiet Ng <kkng33102@gmail.com>
2025-12-18 17:49:02 +0900

    adding dialog manager for future use.

--- Assets/Scripts/Misc/Npc/DialogBoxLogic.cs
+++ Assets/Scripts/Misc/Npc/DialogBoxLogic.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 
 public class DialogBoxLogic : MonoBehaviour
 {
-    public FreeRoamObjectBehavior objectBehavior;
-    public Unit white_npc;
-    public SignalItem contextOn;
-    public SignalItem contextOff;
-    public GameObject dialogueBox;
-    public TMP_Text dialogText;
-    public string dialog;
-    public bool dialogActive;
     public string popUp;
     bool playerInRange = false;
 
